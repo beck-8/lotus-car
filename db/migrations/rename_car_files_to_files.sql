4e7d872ad3b4bbb16f235b1ecb427585181ede0f
Max <max@coinsummer.io>
2024-12-30 00:20:27 +0800

    fix: migration

--- db/migrations/rename_car_files_to_files.sql
+++ db/migrations/rename_car_files_to_files.sql
@@ -1,16 +1,3 @@
--- Rename the car_files table to files
-ALTER TABLE IF EXISTS car_files RENAME TO files;
-
--- Update the foreign key constraint name
-ALTER TABLE files 
-    RENAME CONSTRAINT IF EXISTS car_files_deal_id_fkey 
-    TO files_deal_id_fkey;
-
--- Update the primary key constraint name
-ALTER TABLE files 
-    RENAME CONSTRAINT IF EXISTS car_files_pkey 
-    TO files_pkey;
-
 -- Add deal_id column if it doesn't exist
 DO $$ 
 BEGIN 
